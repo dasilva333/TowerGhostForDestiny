41e5a9f8b6e1c1ee357d70f1bd961d549ed8b81e
Richard Pinedo <richard.pinedo@yahoo.com>
2015-04-29 09:15:26 -0400

    Fix for the Song of Ir Yut

--- www/js/app.js
+++ www/js/app.js
@@ -830,7 +830,7 @@ var app = new (function() {
 					isEquipped: item.isEquipped,
 					isGridComplete: item.isGridComplete,
 					locked: item.locked,
-					description: info.itemName,
+					description: decodeURIComponent(escape(info.itemName)),
 					bucketType: (item.location == 4) ? "Post Master" : DestinyBucketTypes[info.bucketTypeHash],
 					type: info.itemSubType,
 					typeName: info.itemTypeName,
