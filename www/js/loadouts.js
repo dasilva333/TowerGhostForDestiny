c0cfe07c024ba16c909b5dbd01b4b4c9dfadaaa1
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-02 20:14:57 -0400

    2.2.8 extra change

--- www/js/loadouts.js
+++ www/js/loadouts.js
@@ -260,6 +260,12 @@ Loadout.prototype = {
 							}
 						}
 						else {
+							console.log("trying to equip when " + sourceBucket.length);
+							if ( sourceBucket.length == 1 ){
+								return {
+									description: item.description + " will not be moved. There is no item to replace it."
+								}
+							}
 							var itemFound = false;
 							var swapItem = _.filter(_.where(targetBucket, { type: item.type }), getFirstItem(sourceBucketIds, itemFound));
 							swapItem = (swapItem.length > 0) ? swapItem[0] : _.filter(targetBucket, getFirstItem(sourceBucketIds, itemFound))[0];
