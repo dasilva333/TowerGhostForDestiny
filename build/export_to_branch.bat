0ccee885577f90381dbc7f0e4eb5a280e70c567e
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-03 09:18:43 -0400

    update the export branch script

--- build/export_to_branch.bat
+++ build/export_to_branch.bat
@@ -7,6 +7,8 @@ mkdir ..\build\branch\scripts\
 copy scripts\ ..\build\branch\scripts\
 mkdir ..\build\branch\js\
 copy js\ ..\build\branch\js\
+mkdir ..\build\branch\data\
+copy data\*.js ..\build\branch\data\
 mkdir ..\build\branch\css\
 copy css\ ..\build\branch\css\
 cd ..\build
