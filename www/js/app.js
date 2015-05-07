c2b3e1f8baea5402980334cfb8b6ed332e3f5ee8
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-07 14:14:30 -0400

    backing out the changes for issue #46 until I can find a better solution

--- www/js/app.js
+++ www/js/app.js
@@ -1405,17 +1405,15 @@ var app = new (function() {
 	
 	this.shiftViewLeft = function(){
 		var newIndex = parseInt(self.activeView()) - 1;
-		if (newIndex < 0) newIndex = 3;
+		if (newIndex <= 0) newIndex = 3;
 		self.activeView(newIndex);
-		$.toaster({ priority : 'info', title : 'View Changed', message : 'Set to ' + DestinyViews[newIndex] });
 		self.scrollToActiveIndex();
 	}
 	
 	this.shiftViewRight = function(){
 		var newIndex = parseInt(self.activeView()) + 1;
-		if (newIndex == 4) newIndex = 0;
+		if (newIndex == 4) newIndex = 1;
 		self.activeView(newIndex);
-		$.toaster({ priority : 'info', title : 'View Changed', message : 'Set to ' + DestinyViews[newIndex] });
 		self.scrollToActiveIndex();
 	}
 
@@ -1538,11 +1536,11 @@ var app = new (function() {
 		  }
 		})();
 
-		//if (isMobile){
+		if (isMobile){
 			Hammer(document.getElementById('charactersContainer'))
 				.on("swipeleft", self.shiftViewLeft)
 				.on("swiperight", self.shiftViewRight);
-		//}
+		}
 
 		if (isMobile) {
 		    if (window.device && device.platform === "iOS" && device.version >= 7.0) {
