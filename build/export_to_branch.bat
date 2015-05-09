55e0933a334f5c19a7d82cc1b5bd88a6c2ce1b9d
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-08 20:20:48 -0400

    build changes

--- build/export_to_branch.bat
+++ build/export_to_branch.bat
@@ -13,6 +13,10 @@ mkdir ..\build\branch\data\
 copy data\*.js ..\build\branch\data\
 mkdir ..\build\branch\css\
 copy css\ ..\build\branch\css\
+mkdir ..\build\branch\res\icon\%1
+copy res\icon\%1\ ..\build\branch\res\icon\%1
+mkdir ..\build\branch\res\screen\%1
+copy res\screen\%1\ ..\build\branch\res\screen\%1
 cd ..\build
 xcopy branch ..\..\branches\%1\www\ /E /Y
 rd /s /q branch
