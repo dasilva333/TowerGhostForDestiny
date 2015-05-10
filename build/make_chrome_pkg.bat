5c17433ceb048c02ddf6bda35de157892a548cd9
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-09 20:34:52 -0400

    build process fixes

--- build/make_chrome_pkg.bat
+++ build/make_chrome_pkg.bat
@@ -1,4 +1,4 @@
 del tower_ghost.zip
 cd ..
-"C:\Program Files\WinRAR\WinRAR.exe" a -r -x.git -xbuild -xwww\res build\tower_ghost.zip .
+"C:\Program Files\WinRAR\WinRAR.exe" a -r -x.git -xbuild -xassets -x.svn -xwww\res build\tower_ghost.zip .
 cd build
--- build/make_packages.bat
+++ build/make_packages.bat
@@ -2,3 +2,5 @@ call "make_firefox_pkg.bat"
 timeout 1
 rd /s /q ..\www\temp
 call "make_chrome_pkg.bat"
+move /Y tower_ghost.xpi ..\..\branches\div-movile-dist\tower_ghost.xpi
+move /Y tower_ghost.zip ..\..\branches\div-movile-dist\tower_ghost.zip
