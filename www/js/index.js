c6ae33adb2b4100aa9b20ccd4774abf1ce461c48
Richard Pinedo <richard.pinedo@yahoo.com>
2015-05-12 19:13:34 -0400

    2.5.4 for ios

--- www/js/index.js
+++ www/js/index.js
@@ -14,26 +14,26 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {        
-		var ref = window.open('https://www.bungie.net/en/User/SignIn/Wlid', '_blank', 'location=yes');
-		var loop, cookie;
-		ref.addEventListener('loadstop', function(event) {
-		    clearInterval(loop);
-		    loop = setInterval(function() {
-		        ref.executeScript({
-		            code: 'document.cookie'
-		        }, function(result) {
-		            //alert('received: ' + result);
-					cookie = result;
-		        });
-		    }, 500);
-		});
-		ref.addEventListener('loadstart', function(event) {
-		    clearInterval(loop);
-		});
-		ref.addEventListener('exit', function() {
-			alert("final result is " + cookie);
-		});
+    onDeviceReady: function() {
+        var ref = window.open('https://www.bungie.net/en/User/SignIn/Wlid', '_blank', 'location=yes');
+        var loop, cookie;
+        ref.addEventListener('loadstop', function(event) {
+            clearInterval(loop);
+            loop = setInterval(function() {
+                ref.executeScript({
+                    code: 'document.cookie'
+                }, function(result) {
+                    //alert('received: ' + result);
+                    cookie = result;
+                });
+            }, 500);
+        });
+        ref.addEventListener('loadstart', function(event) {
+            clearInterval(loop);
+        });
+        ref.addEventListener('exit', function() {
+            alert("final result is " + cookie);
+        });
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
