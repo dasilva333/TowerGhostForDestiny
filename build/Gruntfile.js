1a7316aba6aaa07ecbfd26313625f9b54bfbbd73
Zach Reed <zreed@bluetideproductions.com>
2015-05-14 12:35:49 -0500

    Tons of design polish

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -8,8 +8,10 @@ module.exports = function(grunt) {
 
 		project: {
 			app: ['../www'],
+			gen_css: ['<%= project.app %>/css/style_new.css'],
 			css: ['<%= project.app %>/scss/style_new.scss']
 		},
+
 		sass: {
 			dev: {
 				options: {
@@ -21,16 +23,30 @@ module.exports = function(grunt) {
 				}
 			}
 		},
+
 		watch: {
 			sass: {
 				files: '<%= project.app %>/scss/{,*/}*.{scss,sass}',
-				tasks: ['sass:dev']
+				tasks: ['sass:dev', 'autoprefixer:dev']
+			}
+		},
+
+		autoprefixer: {
+			dev: {
+				options: {
+					map: true,
+					browsers: ['> 5%', 'last 20 versions', 'Firefox ESR', 'Opera 12.1', 'ie 9', 'ie 10', 'ie 7']
+				},
+				src: '../www/css/style_new.css',
+				dest: '../www/css/style_new.css'
 			}
 		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
 
 	grunt.registerTask('default', [
 		'watch'
