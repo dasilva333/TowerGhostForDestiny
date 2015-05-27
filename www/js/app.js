d9570613b583536cbe8b107243adc90c31572eed
dasilva333 <richard.pinedo@yahoo.com>
2015-05-26 21:58:26 -0400

    2.6.5 for ios

--- www/js/app.js
+++ www/js/app.js
@@ -214,6 +214,11 @@ var app = new(function() {
     this.activeLoadout = ko.observable(new Loadout());
     this.loadouts = ko.observableArray();
     this.searchKeyword = ko.observable(tgd.defaults.searchKeyword);
+    this.vaultPos = ko.computed(new tgd.StoreObj("vaultPos"));
+    this.xsColumn = ko.computed(new tgd.StoreObj("xsColumn"));
+    this.smColumn = ko.computed(new tgd.StoreObj("smColumn"));
+    this.mdColumn = ko.computed(new tgd.StoreObj("mdColumn"));
+    this.lgColumn = ko.computed(new tgd.StoreObj("lgColumn"));
     this.activeView = ko.computed(new tgd.StoreObj("activeView"));
     this.doRefresh = ko.computed(new tgd.StoreObj("doRefresh", "true"));
     this.autoTransferStacks = ko.computed(new tgd.StoreObj("autoTransferStacks", "true"));
@@ -246,7 +251,7 @@ var app = new(function() {
     this.characters = ko.observableArray();
     this.orderedCharacters = ko.computed(function() {
         return self.characters().sort(function(a, b) {
-            return a.order - b.order;
+            return a.order() - b.order();
         });
     });
 
@@ -624,7 +629,7 @@ var app = new(function() {
                     var buckets = results.data.buckets;
                     var profile = new Profile({
                         race: "",
-                        order: 0,
+                        order: self.vaultPos(),
                         gender: "Tower",
                         classType: "Vault",
                         id: "Vault",
@@ -1229,6 +1234,59 @@ var app = new(function() {
         }
 
         nextNormalize();
+    }
+
+    this.setVaultTo = function(pos) {
+        return function() {
+            var vault = _.findWhere(self.characters(), {
+                id: "Vault"
+            });
+            if (vault) {
+                self.vaultPos(pos);
+                vault.order(pos);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    this.isVaultAt = function(pos) {
+        return ko.computed(function() {
+            var vault = _.findWhere(self.characters(), {
+                id: "Vault"
+            });
+            if (vault) {
+                result = (vault.order() == pos);
+            } else {
+                result = false;
+            }
+            return result;
+        }).extend({
+            rateLimit: {
+                timeout: 1000,
+                method: "notifyWhenChangesStop"
+            }
+        });
+    }
+
+    this.columnMode = ko.computed(function() {
+        return "col-xs-" + self.xsColumn() + " col-sm-" + self.smColumn() + " col-md-" + self.mdColumn() + " col-lg-" + self.lgColumn();
+    });
+
+    this.setColumns = function(type, input) {
+        return function() {
+            self[type + "Column"](12 / input.value);
+        }
+    }
+
+    this.btnActive = function(type, input) {
+        return ko.computed(function() {
+            return ((12 / input.value) == self[type + "Column"]()) ? "btn-primary" : "";
+        });
+    };
+
+    this.viewOptions = function() {
+        $("#viewOptions").toggle();
     }
 
     this.init = function() {
