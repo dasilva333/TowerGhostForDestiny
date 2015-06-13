10b2b0997b0f41605f99b83a67462f7dec8d6204
dasilva333 <richard.pinedo@yahoo.com>
2015-06-13 11:50:24 -0400

    2.7.2 for ios

--- www/data/tgdDefs.js
+++ www/data/tgdDefs.js
@@ -87,6 +87,8 @@ tgd.defaults = {
 	appLocale: "",
 	//internally cached version of the itemDefs
 	defsLocale: "en",
+	//as of 2.7.0 I added versioning to itemDefs so the default would be this for everyone
+	defLocaleVersion: "2.7.0",
 	vaultPos: 0,
 	itemDefs: "",
 	preferredSystem: "PSN"
