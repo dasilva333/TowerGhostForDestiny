2e651200095626104025ddf92ce1c889147305f2
Richard Pinedo <richard.pinedo@yahoo.com>
2015-06-17 11:52:59 -0400

    more fixes

--- www/js/app.js
+++ www/js/app.js
@@ -1393,7 +1393,12 @@ var app = new(function() {
     this.initItemDefs = function() {
         var itemDefs = self.itemDefs();
         if (self.currentLocale() != "en" && !_.isEmpty(itemDefs) && self.currentLocale() == self.defsLocale()) {
-            window._itemDefs = JSON.parse(itemDefs);
+			try {
+				window._itemDefs = JSON.parse(itemDefs);
+			}catch(e){
+				console.log("invalid itemDefs");
+				self.itemDefs("");
+			}
         }
     }
 
