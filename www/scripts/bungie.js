68841999477107bd9304f30f7a634872d65ff52f
dasilva333 <richard.pinedo@yahoo.com>
2015-06-28 00:23:44 -0400

    possible fix for issue 58

--- www/scripts/bungie.js
+++ www/scripts/bungie.js
@@ -19,7 +19,6 @@ try {
 		window.addEventListener("message", function(event) {
 			console.log("received a firefox response");			
 			var reply = event.data;
-			console.log(reply);
 			var response = JSON.parse(reply.response);
 			var opts = requests[reply.id];			
 			if(response.ErrorCode === 36){ 
@@ -117,11 +116,13 @@ try {
 		    r.open(opts.method, url + "Platform" + opts.route, true);
 		    r.setRequestHeader('X-API-Key', apikey);
 		    r.onload = function() {
-			  var response = this.response;
+			  var response;
 			  //console.timeEnd("XMLHttpRequest"); 
 			  try {
-			  	response = JSON.parse(this.response);
-			  }catch(e){}		  
+			  	response = JSON.parse(this.responseText);
+			  }catch(e){
+			  	console.log("error parsing responseText: " + this.responseText);
+			  }		  
 		      if (this.status >= 200 && this.status < 400) {	        		
 			        if(response.ErrorCode === 36){ setTimeout(function () { _request(opts); }, 1000); }
 			        else { opts.complete(response.Response, response); }			
