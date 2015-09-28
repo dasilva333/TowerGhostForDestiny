d5c3af831d7f483c5e831c9d1fc6195e458cda80
Richard Pinedo <richard.pinedo@yahoo.com>
2015-09-28 14:40:00 -0400

    fix for green weapons like DED VENEFICI showing double perks

--- www/js/app.js
+++ www/js/app.js
@@ -621,7 +621,7 @@ var app = new(function() {
                 }
                 /* Weapon Perks (Post-HoW) */
                 else if (activeItem.perks.length > 0 && $content.find(".destt-talent").length == 0) {
-                    $content.find(".destt-info").before(tgd.perksTemplate({
+                    $content.find(".destt-info:last").before(tgd.perksTemplate({
                         perks: activeItem.perks
                     }));
                 }
