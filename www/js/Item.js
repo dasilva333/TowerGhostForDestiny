49b0cb380ab4227d6430ed33f261db34c3d9ece1
Richard Pinedo <richard.pinedo@yahoo.com>
2015-10-06 11:57:01 -0400

    adding telemetries to the general filter

--- www/js/Item.js
+++ www/js/Item.js
@@ -253,6 +253,9 @@ Item.prototype = {
         else if (type == "Glimmer Consumables" && [3446457162, 1043138475, 1772853454, 3783295803].indexOf(this.id) > -1) {
             return true;
         }
+        else if (type == "Telemetries" && [4159731660,729893597,3371478409,927802664,4141501356,323927027,3036931873,2610276738,705234570,1485751393,2929837733,846470091].indexOf(this.id) > -1) {
+            return true;
+        }
     },
     _primaryStatValue: function() {
         if (this.primaryStat && typeof this.primaryStat == "function") {
