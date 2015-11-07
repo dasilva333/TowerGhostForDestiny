7049c4c907c78ad7439cfd42465eca7068d482fb
dasilva333 <richard.pinedo@yahoo.com>
2015-11-07 10:05:40 -0500

    fixes for the background image

--- www/js/extras/autoupdate.js
+++ www/js/extras/autoupdate.js
@@ -29,7 +29,7 @@
         Promise: Promise
     });
 
-    loader = new CordovaAppLoader({
+    tgd.loader = new CordovaAppLoader({
         fs: fs,
         localRoot: 'app',
         serverRoot: serverRoot,
@@ -38,23 +38,23 @@
     });
 
     // Check > Download > Update
-    function check() {
+    tgd.check = function() {
         console.log("RUNNING CHECK");
-        loader.check()
+        tgd.loader.check()
             .then(function() {
                 console.log("RUNNING DOWNLOAD");
-                return loader.download();
+                return tgd.loader.download();
             })
             .then(function() {
                 console.log("RUNNING UPDATE");
-                return loader.update();
+                return tgd.loader.update();
             }, function(err) {
                 console.error('Auto-update error:', err);
             });
     }
 
     // Couple events:
-    check();
+    tgd.check();
 
     // 2. Cordova: On resume
     fs.deviceready.then(function() {
@@ -64,7 +64,7 @@
     // 3. Chrome: On page becomes visible again
     function handleVisibilityChange() {
         if (!document.webkitHidden) {
-            check();
+            tgd.check();
         }
     }
     document.addEventListener("webkitvisibilitychange", handleVisibilityChange, false);
