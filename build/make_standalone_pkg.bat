4c75dd2ebce2f6d34c0dd02e6954005292edb747
dasilva333 <richard.pinedo@yahoo.com>
2015-11-07 19:34:30 -0500

    fixes for firefox/nwjs

--- build/make_standalone_pkg.bat
+++ build/make_standalone_pkg.bat
@@ -7,8 +7,8 @@ cp js temp\www -R
 cp data temp\www -R
 cp scripts temp\www -R
 cp lib temp -R
-cp *.html temp\www
-cp manifest.json temp\www
+cp index.html temp\www
+cp bootstrap.json temp\www
 copy icon.png temp
 copy ..\package_nw.json temp\package.json
 cd ..\build\nwjs
