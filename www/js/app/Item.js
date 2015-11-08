8e212e41382673841f5fdd2ccff99358ee844d73
dasilva333 <richard.pinedo@yahoo.com>
2015-11-07 21:41:13 -0500

    fix for the exotic class items w the life exotic perk

--- www/js/app/Item.js
+++ www/js/app/Item.js
@@ -486,7 +486,7 @@ Item.prototype = {
         if (targetCharacterId == sourceCharacterId) {
             tgd.localLog("item is already in the character");
             /* if item is exotic */
-            if (self.tierType == 6) {
+            if (self.tierType == 6 && _.where(self.perks,{name: "The Life Exotic"}).length === 0) {
                 //tgd.localLog("item is exotic");
                 var otherExoticFound = false,
                     otherBucketTypes = self.weaponIndex > -1 ? _.clone(tgd.DestinyWeaponPieces) : _.clone(tgd.DestinyArmorPieces);
