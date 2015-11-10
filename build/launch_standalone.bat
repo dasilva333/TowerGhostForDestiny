e579d1e65d086d4b189b1406715ec490003e99e5
Richard Pinedo <richard.pinedo@yahoo.com>
2015-11-10 16:53:06 -0500

    changing standalone loader for debug

---
+++ build/launch_standalone.bat
@@ -0,0 +1,5 @@
+cd ..
+copy package_nw.json package.json
+build\nwjs\download\nwjs-v0.12.1-win-x64\nwjs-v0.12.1-win-x64\nw.exe .
+rm package.json
+cd build
--- launch.bat
+++
@@ -1,1 +0,0 @@
-build\nwjs-v0.12.3-win-x64\nw.exe .
