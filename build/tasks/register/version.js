c7b376f580036c84eb1542b65a4d517f6022156c
Richard Pinedo <richard.pinedo@yahoo.com>
2015-11-12 13:57:47 -0500

    now the xml supports 4 digit and 3 digit version codes

--- build/tasks/register/version.js
+++ build/tasks/register/version.js
@@ -28,9 +28,11 @@ module.exports = function (grunt) {
 		_.each(options.platforms, function(platform){
 			var adobeBuildConfigFile = "../www/config_" + platform + ".xml";
 			var xmlConfig = grunt.file.read(adobeBuildConfigFile).toString("utf8");
+			var versionCode = versionInfo.replace(/\./g,'');
+			if (versionCode.length == 3) versionCode = versionCode + "0";
 			//avoid having to load xml libraries to update it
 			xmlConfig = xmlConfig.replace(/version="(.*)" xmlns=\"http:\/\/www.w3.org\/ns\/widgets\"/,'version="' + versionInfo + '" xmlns="http://www.w3.org/ns/widgets"');
-			xmlConfig = xmlConfig.replace(/id=\"com.richardpinedo.towerghostfordestiny\" versionCode="(.*)" version/,'id=\"com.richardpinedo.towerghostfordestiny\" versionCode="' + versionInfo.replace(/\./g,'') + '" version');
+			xmlConfig = xmlConfig.replace(/id=\"com.richardpinedo.towerghostfordestiny\" versionCode="(.*)" version/,'id=\"com.richardpinedo.towerghostfordestiny\" versionCode="' + versionCode + '" version');
 			grunt.file.write(adobeBuildConfigFile, xmlConfig);
 		});
 
