f7eab1494bc7ebf55182e44b1047b3ebb13a8685
Richard Pinedo <richard.pinedo@yahoo.com>
2015-11-18 10:59:10 -0500

    production is 3 digits, auto updates is 4 digits

--- build/tasks/register/version.js
+++ build/tasks/register/version.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
 			var xmlConfig = grunt.file.read(adobeBuildConfigFile).toString("utf8");
 			var versionCode = threeDigitVersion.replace(/\./g,'') + '0';
 			//avoid having to load xml libraries to update it
-			xmlConfig = xmlConfig.replace(/version="(.*)" xmlns=\"http:\/\/www.w3.org\/ns\/widgets\"/,'version="' + versionInfo + '" xmlns="http://www.w3.org/ns/widgets"');
+			xmlConfig = xmlConfig.replace(/version="(.*)" xmlns=\"http:\/\/www.w3.org\/ns\/widgets\"/,'version="' + threeDigitVersion + '" xmlns="http://www.w3.org/ns/widgets"');
 			xmlConfig = xmlConfig.replace(/id=\"com.richardpinedo.towerghostfordestiny\" versionCode="(.*)" version/,'id=\"com.richardpinedo.towerghostfordestiny\" versionCode="' + versionCode + '" version');
 			grunt.file.write(adobeBuildConfigFile, xmlConfig);
 		});
