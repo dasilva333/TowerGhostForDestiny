82e7330aefd21f53f98ed6eceeb7c3d13b7b6518
dasilva333 <richard.pinedo@yahoo.com>
2015-11-19 00:17:52 -0500

    static share ready

--- www/js/tgd/base.js
+++ www/js/tgd/base.js
@@ -7,7 +7,7 @@ window.isAndroid = (/android/i.test(ua));
 window.isWindowsPhone = (/iemobile/i.test(ua));
 window.isMobile = (window.isIOS || window.isAndroid || window.isWindowsPhone);
 window.isKindle = /Kindle/i.test(ua) || /Silk/i.test(ua) || /KFTT/i.test(ua) || /KFOT/i.test(ua) || /KFJWA/i.test(ua) || /KFJWI/i.test(ua) || /KFSOWI/i.test(ua) || /KFTHWA/i.test(ua) || /KFTHWI/i.test(ua) || /KFAPWA/i.test(ua) || /KFAPWI/i.test(ua);
-window.isStaticBrowser = true;
+window.isStaticBrowser = false;
 window.tgd = {};
 if ( isWindowsPhone ){
 	window.requestFileSystem = function(){}
