582ee015c46284b311cd252c359a64d238691186
Richard Pinedo <richard.pinedo@yahoo.com>
2015-12-07 15:55:08 -0500

    build process tweak to accomodate Apple's naming scheme

--- build/applyCurrentVersion.js
+++ build/applyCurrentVersion.js
@@ -11,6 +11,9 @@ var adobeBuildOutputConfigFile = __dirname + "/config_" + platform + "_versioned
 var versionInfo = fs.readFileSync(__dirname + "/version.txt").toString("utf8");
 var xmlConfig = fs.readFileSync(adobeBuildConfigFile).toString("utf8");
 var versionCode = versionInfo.replace(/\./g,'');
+if ( platform == "ios" ){
+	versionInfo = versionInfo.substring(0,5);
+}
 //avoid having to load xml libraries to update it
 xmlConfig = xmlConfig.replace(/version="(.*)" xmlns=\"http:\/\/www.w3.org\/ns\/widgets\"/,'version="' + versionInfo + '" xmlns="http://www.w3.org/ns/widgets"');
 xmlConfig = xmlConfig.replace(/id=\"com.richardpinedo.towerghostfordestiny\" versionCode="(.*)" version/,'id=\"com.richardpinedo.towerghostfordestiny\" versionCode="' + versionCode + '" version');
