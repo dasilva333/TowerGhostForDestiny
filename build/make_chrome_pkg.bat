c908e35d649a62fe5b7d050d8e7ca3af84db2d41
dasilva333 <richard.pinedo@yahoo.com>
2015-12-26 13:15:41 -0500

    3.7.5.3 - Added a holiday message to the transfer dialog

--- build/make_chrome_pkg.bat
+++ build/make_chrome_pkg.bat
@@ -1,4 +1,4 @@
 del tower_ghost.zip
 cd ..
-"C:\Program Files\WinRAR\WinRAR.exe" a -r -x.git -xbuild -xsource -x.svn -xwww\templates -xwww\spec -xwww\res -xwww\css -xwww\temp -xwww\js -xwww\resources\*.gz -xwww\data\definitions build\tower_ghost.zip .
+"C:\Program Files\WinRAR\WinRAR.exe" a -r -x.git -xbuild -xsource -x.svn -xwww\templates -xwww\spec -xwww\res -xwww\css -xwww\temp -xwww\js -xwww\resources\de -xwww\resources\es -xwww\resources\fr -xwww\resources\it -xwww\resources\ja -xwww\resources\pt-br -xwww\resources\*.gz -xwww\data\definitions build\tower_ghost.zip .
 cd build
