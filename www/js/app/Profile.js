fc2d8ec6c90746464bd731d34ae3d8a08ea6f941
Richard Pinedo <richard.pinedo@yahoo.com>
2016-04-08 09:24:19 -0400

    revert fix for double exotics until a better solution can be implemented

--- www/js/app/Profile.js
+++ www/js/app/Profile.js
@@ -731,11 +731,10 @@ Profile.prototype = {
                     return subSets;
                 });
                 _.each(arrRolls, function(subSets) {
-                    candidates = _.filter(backups, function(item) {
-                        return item.bucketType != mainPiece.bucketType && (item.tierType != 6 || (item.tierType == 6 && item.hasLifeExotic == true)) && mainPiece._id != item._id;
-                    });
-                    var groupedCandidates = _.groupBy(candidates, 'bucketType');
-                    _.each(groupedCandidates, function(items) {
+                    candidates = _.groupBy(_.filter(backups, function(item) {
+                        return item.bucketType != mainPiece.bucketType && ((item.tierType != 6 && mainPiece.tierType == 6) || (mainPiece.tierType != 6)) && mainPiece._id != item._id;
+                    }), 'bucketType');
+                    _.each(candidates, function(items) {
                         subSets.push(items);
                     });
                     var combos = tgd.cartesianProductOf(subSets);
