e8787bce0869a0d30b7d768f5433caf967a25074
dasilva333 <richard.pinedo@yahoo.com>
2016-05-31 09:50:26 -0400

    pushed to au

--- build/tasks/register/prod.js
+++ build/tasks/register/prod.js
@@ -1,6 +1,7 @@
 module.exports = function (grunt) {
 	grunt.registerTask('prod', 		['generate-version:show','codeformat','combine-templates','removelogging:js','uglify:js','concat_sourcemap:css','cssmin','jsonmanifest:prod']);
-	grunt.registerTask('prod-wp', 	['generate-version:show','codeformat','combine-templates','removelogging:wp','uglify:wp','concat_sourcemap:css','cssmin','jsonmanifest:prod']);	
+	grunt.registerTask('prod-wp', 	['generate-version:show','codeformat','combine-templates','removelogging:wp','uglify:wp','concat_sourcemap:css','cssmin','jsonmanifest:prod']);
+	grunt.registerTask('prod-share',['generate-version:show','codeformat','combine-templates','removelogging:wp','uglify:wp','concat_sourcemap:css','cssmin','jsonmanifest:share']);
 	grunt.registerTask('prod-ff', 	['generate-version:show','codeformat','combine-templates','removelogging:js','uglify:js','concat_sourcemap:css','cssmin','jsonmanifest:firefox']);	
 	grunt.registerTask('prod-def',	['generate-version:show','codeformat','combine-templates','removelogging:js','uglify:js', 'uglify:definitions','cssmin','jag:definitions','jsonmanifest:prod']);
 };
