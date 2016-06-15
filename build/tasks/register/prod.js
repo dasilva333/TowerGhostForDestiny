a42ae4dd3f8889eba19a9ebf44ff01d39785f492
Richard Pinedo <richard.pinedo@yahoo.com>
2016-06-15 16:11:53 -0400

    adding a new prod-dev task to create a production version with logging

--- build/tasks/register/prod.js
+++ build/tasks/register/prod.js
@@ -1,5 +1,6 @@
 module.exports = function (grunt) {
 	grunt.registerTask('prod', 		['generate-version:show','codeformat','combine-templates','removelogging:js','uglify:js','concat_sourcemap:css','cssmin','jsonmanifest:prod']);
+	grunt.registerTask('prod-dev',	['generate-version:show','codeformat','combine-templates','uglify:js','uglify:jsDev','concat_sourcemap:css','cssmin','jsonmanifest:prod']);
 	grunt.registerTask('prod-wp', 	['generate-version:show','codeformat','combine-templates','removelogging:wp','uglify:wp','concat_sourcemap:css','cssmin','jsonmanifest:prod']);
 	grunt.registerTask('prod-share',['generate-version:show','codeformat','combine-templates','removelogging:wp','uglify:wp','concat_sourcemap:css','cssmin','jsonmanifest:share']);
 	grunt.registerTask('prod-ff', 	['generate-version:show','codeformat','combine-templates','removelogging:js','uglify:js','concat_sourcemap:css','cssmin','jsonmanifest:firefox']);	
