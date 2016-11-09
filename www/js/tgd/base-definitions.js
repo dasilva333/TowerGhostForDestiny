7c7d127d0a090412964151f6c729091a2a3ba1f4
Richard Pinedo <richard.pinedo@yahoo.com>
2016-11-09 16:43:51 -0500

    compiled

--- www/js/tgd/base-definitions.js
+++ www/js/tgd/base-definitions.js
@@ -27,8 +27,8 @@ tgd.localLog = function(msg) {
 function handleOpenURL(link) {
     if (link && link.indexOf && link.indexOf("code") > -1) {
         tgd.bungieCode = link.split("=")[1];
-        if ( app && app.bungie ){
+        if (app && app.bungie) {
             app.bungie.loginWithCode(tgd.bungieCode, app.activeUser);
-        }        
+        }
     }
 }
